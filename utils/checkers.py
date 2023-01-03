b8c09ebe57807f042f085953a9ad1b648c951071
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2023-01-03 06:12:43 +0000

    init files

--- utils/checkers.py
+++ utils/checkers.py
@@ -11,6 +11,16 @@ from ml_collections.config_dict import ConfigDict
 logger = logging.getLogger(__name__)
 
 
+def create_init(path: str):
+    """Create the init file.
+
+    Args:
+        path (str): path where we want to create the init file.
+    """
+    with open(path + "__init__.py", "w+") as file:
+        file.write('import os')
+
+
 def make_paths(config: ConfigDict) -> None:
     """Make sure all relevant folders where we store outputs exist.
     Args:
@@ -21,3 +31,7 @@ def make_paths(config: ConfigDict) -> None:
     os.makedirs(config.path.data, exist_ok=True)
     os.makedirs(config.path.plots, exist_ok=True)
     os.makedirs(config.path.logs, exist_ok=True)
+
+    create_init(config.path.data)
+    create_init(config.path.plots)
+    create_init(config.path.logs)
